3f67d1d9a49ac2706ebb073220a0d1f105d9670e
Sacochee <127160567+Sacochee@users.noreply.github.com>
2023-11-08 14:12:19 +0100

    tabl desktop

--- app/[locale]/art/[id]/vente.js
+++ app/[locale]/art/[id]/vente.js
@@ -3,9 +3,48 @@ import style from "./style.module.css";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useMediaQuery } from "@/compoments/hooks";
+import desktop from "./desktop.module.css"
 
 export default function Vente({ data }) {
   const lst = data.images.split(";") ? data.images.split(";") : [data.images];
+  
+  return (
+    <main>
+      <div className={style.center}>
+        <h1 className={style.title}>{data.name}</h1>
+      </div>
+      <div className={style.cadre}>
+        <section className={`${style.center} ${style.container}`}>
+          {useMediaQuery(1023) ? <ImagesMobile lst={lst} data={data}/> : <ImageDesktop data={lst}/>}
+        </section>
+        <section className={style.cont}>
+          <div className={style.part1}>
+            <div className={style.price}>{data.price}€</div>
+            <div className={style.size}>
+              <div className={style.flex}>
+                <div className={style.height}>{data.heigth}</div>
+                <div className={style.carre}></div>
+              </div>
+              <div className={style.flex}>
+                <div className={style.cm}>Cm</div>
+                <div className={style.width}>{data.width}</div>
+              </div>
+            </div>
+          </div>
+          <button className={style.btn}>Buy</button>
+          <p className={style.desc}>{data.description}</p>
+        </section>
+      </div>
+      <section>
+          <h2>
+            faq 2 vente 
+          </h2>
+        </section>
+    </main>
+  );
+}
+
+function ImagesMobile({data, lst}){
   const [img, setImg] = useState(0);
   const [full, setFull] = useState(false);
   const change = (i) => {
@@ -13,96 +52,60 @@ export default function Vente({ data }) {
       setImg(img + i);
     } else setImg(0);
   };
-  return (
-    <main>
-      {full ? (
-        <Container>
-          <div onClick={() => setFull(false)} className={style.over}>
-            {screen.availWidth < screen.availHeight ? <div className={style.top} style={{height : (screen.availHeight) - screen.availWidth - 150}}></div> : undefined}
-            <Full data={lst[img]} />
-            <div className={style.bg}></div>
-          </div>
-        </Container>
-      ) : (
-        <>
-          <div className={style.center}>
-            <h1 className={style.title}>{data.name}</h1>
-          </div>
-          <section className={`${style.center} ${style.container}`}>
-            <div className={style.box}>
-              {lst.length == 1 ? undefined : (
-                <div onClick={() => change(-1)} className={style.flecheGauche}>
-                  <Image
-                    src={"/images/left.png"}
-                    width={40}
-                    height={40}
-                    alt="fleche qui pointe vers la gauche"
-                  />
-                </div>
-              )}
-              <Image
-                src={`http://195.35.48.48:8080/${lst[img]}`}
-                width={270}
-                height={270}
-                alt={data.name + "de patricia teyssier"}
-                className={style.img}
-                onClick={() => setFull(true)}
-              />
-              {lst.length == 1 ? undefined : (
-                <div onClick={() => change(+1)} className={style.flecheDroite}>
-                  <Image
-                    src={"/images/right.png"}
-                    width={40}
-                    height={40}
-                    alt="fleche qui pointe vers la droite"
-                  />
-                </div>
-              )}
-            </div>
-          </section>
-          <section className={style.cont}>
-            <div className={style.part1}>
-              <div className={style.price}>{data.price}€</div>
-              <div className={style.size}>
-                <div className={style.flex}>
-                  <div className={style.height}>{data.heigth}</div>
-                  <div className={style.carre}></div>
-                </div>
-                <div className={style.flex}>
-                  <div className={style.cm}>Cm</div>
-                  <div className={style.width}>{data.width}</div>
-                </div>
-              </div>
-            </div>
-            <button className={style.btn}>Buy</button>
-            <p>{data.description}</p>
-          </section>
-        </>
+  return(
+    <div className={style.box}>
+      {lst.length == 1 ? undefined : (
+        <div onClick={() => change(-1)} className={style.flecheGauche}>
+          <Image
+            src={"/images/left.png"}
+            width={40}
+            height={40}
+            alt="fleche qui pointe vers la gauche"
+          />
+        </div>
       )}
-    </main>
-  );
-}
-
-function Full({ data }) {
-  return (
-    <div
-      style={{ width: screen.width }}
-      className={`${style.center}  ${style.full}`}
-    >
       <Image
-        src={`http://195.35.48.48:8080/${data}`}
-        width={screen.width < screen.height ? screen.width : screen.height}
-        height={screen.width < screen.height ? screen.width : screen.height}
-        alt="tableaux de patricial en full Screen"
-        className={style.full}
+        src={`http://195.35.48.48:8080/${lst[img]}`}
+        width={270}
+        height={270}
+        alt={data.name + "de patricia teyssier"}
+        className={style.img}
+        onClick={() => setFull(true)}
       />
+      {lst.length == 1 ? undefined : (
+        <div onClick={() => change(+1)} className={style.flecheDroite}>
+          <Image
+            src={"/images/right.png"}
+            width={40}
+            height={40}
+            alt="fleche qui pointe vers la droite"
+          />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-function Container({ children }) {
-  useEffect(() => {
-    scrollBy(0, 0)
-  });
-  return <div>{children}</div>;
+function ImageDesktop({data : lst}){
+  const [img, setImg] = useState(lst[0])
+  return(
+    <div className={desktop.main}>
+      <div className={desktop.part}>
+        {lst.map((item)=><Image 
+        src={`http://195.35.48.48:8080/${item}`}
+        width={100}
+        height={100}
+        onClick={()=>setImg(item)}
+        className={desktop.select}
+        />)}
+      </div>
+      <div className={desktop.img}>
+        <Image 
+        src={`http://195.35.48.48:8080/${img}`}
+        width={600}
+        height={600}
+        />
+      </div>
+    </div>
+  )
 }
